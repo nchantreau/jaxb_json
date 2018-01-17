2c33863a260176d20f2017885e12b2d2ce3c2b47
Nicolas Chantreau <citrouille86@gmail.com>
2018-01-17 09:02:23 +0100

    Correction de la validation XSD

--- src/main/java/fr/apside/pizza/Main.java
+++ src/main/java/fr/apside/pizza/Main.java
@@ -3,6 +3,7 @@ package fr.apside.pizza;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,21 +16,19 @@ import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fr.apside.pizza.exception.ArgumentException;
 import fr.apside.pizza.xml.PizzaOrder;
+import fr.apside.pizza.xml.PizzaType;
 
 public class Main {
 
@@ -55,26 +54,27 @@ public class Main {
 	List<PizzaOrder> listOrders = new ArrayList<>();
 
 	try (Stream<Path> list = Files.list(Paths.get(prop.getProperty("directory.in")));) {
-	    list.forEach(path -> {
-		if (isValid(path)) {
-		    try (InputStream inputStream = Files.newInputStream(path);
-			    OutputStream outputStream = Files.newOutputStream(Paths.get("target/orders.json"));) {
-			// Lecture XML
-			JAXBContext jc = JAXBContext.newInstance(PizzaOrder.class);
-			Unmarshaller unmarshaller = jc.createUnmarshaller();
+	    list.filter(p -> isValid(p)).forEach(path -> {
+		try (InputStream inputStream = Files.newInputStream(path);
+			OutputStream outputStream = Files.newOutputStream(Paths.get("target/orders.json"));) {
+		    // Lecture XML
+		    JAXBContext jc = JAXBContext.newInstance(PizzaOrder.class);
+		    Unmarshaller unmarshaller = jc.createUnmarshaller();
+		    // Deserialisation
+		    PizzaOrder order = (PizzaOrder) unmarshaller.unmarshal(inputStream);
 
-			PizzaOrder order = (PizzaOrder) unmarshaller.unmarshal(inputStream);
+		    listOrders.add(order);
 
-			listOrders.add(order);
+		    // Recuperation de la quantite de la commande
+		    BigInteger qte = order.getPizzas().getPizza().stream().map(PizzaType::getQuantity)
+			    .reduce(BigInteger.ZERO, BigInteger::add);
+		    log.info("Commande de " + qte + " pizzas pour "
+			    + order.getCustomer().getName().getFirstName() + " "
+			    + order.getCustomer().getName().getLastName());
 
-			log.info("Commande de " + order.getPizzas().getPizza().size() + " pizzas pour "
-				+ order.getCustomer().getName().getFirstName() + " "
-				+ order.getCustomer().getName().getLastName());
-
-		    } catch (IOException | JAXBException e) {
-			String msg = "Erreur de lecture de l'XML";
-			log.error(msg, e);
-		    }
+		} catch (IOException | JAXBException e) {
+		    String msg = "Erreur de lecture de l'XML";
+		    log.error(msg, e);
 		}
 	    });
 	}
@@ -93,16 +93,13 @@ public class Main {
 	try {
 	    SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
-	    DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	    Document document = parser.parse(path.toFile());
-
 	    Schema schema = schemaFactory.newSchema(Paths.get("src/main/resources/pizza_order.xsd").toFile());
 	    Validator validator = schema.newValidator();
-//	    TODO validator.validate(new DOMSource(document));
+	    validator.validate(new StreamSource(path.toFile()));
 	    return true;
 	} catch (Exception e) {
 	    log.error("Invalid order: " + path, e);
+	    return false;
 	}
-	return false;
     }
 }
